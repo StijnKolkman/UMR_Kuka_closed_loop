e362ad509524ac1a1bb96a8a715b680e2add76c9
RAM-Micro <ram-micro@ram-micro.nlgit config --global user.name ram-micro@ram-micro.nl>
2025-06-01 20:35:34 +0200

    test

---
+++ Stijn/src/kukavarproxy_ros/launch/stijn_launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
+def generate_launch_description():
+    return LaunchDescription([
+        # 1) Launch your standalone publisher script
+        ExecuteProcess(
+            cmd=[
+                'python3',
+                'Stijn/recorder.py'  # or wherever you placed it
+            ],
+            output='screen'
+        ),
+        Node(
+            package='kukavarproxy_ros',
+            executable='kuka_commander',
+            name='kuka_commander',
+            output='screen'
+        ),
+    ])
